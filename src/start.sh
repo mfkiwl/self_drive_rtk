69c8a0cbd398fc6797f1b8e138c69dd5c49c709a
BranYuan <biaoyuan_shu@163.com>
2020-05-27 17:00:51 +0800

    start.sh scripts update

--- src/start.sh
+++ src/start.sh
@@ -25,15 +25,11 @@ sudo -S chmod 777 /dev/ttyS6 << EOF
 1
 EOF
 
+cd /home/sweet/Desktop/robot/robot
 
-# source /opt/ros/melodic/setup.bash
-# source /home/sweet/github_store/robot_nav/devel/setup.bash
+gnome-terminal --window --title='robot' -- ./robot
+gnome-terminal --window --title='tracking' -- ../tracking/tracking
+gnome-terminal --window --title='track_cmd' -- ../track_cmd/track_cmd
 
-/home/sweet/robot/robot
-
-#echo "this is a test" >> /home/sweet/github_store/robot_nav/src/base_controller/launch/text.log
-
-# roslaunch /home/sweet/github_store/robot_nav/src/base_controller/launch/base_controller.launch
-# roslaunch /home/sweet/github_store/robot_nav/src/robot_arm_config/launch/demo.launch
 
 exit 0
