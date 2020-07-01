56669a2fc6bf7b7d41b6957fe63a05e60284a79f
BranYuan <biaoyuan_shu@163.com>
2020-07-01 19:51:05 +0800

    update

--- src/upgrade.py
+++ src/upgrade.py
@@ -3,19 +3,24 @@
 import subprocess
 import os
 import sys
+import time
 
 
 def run():
     # 运行脚本
     # dst = os.path.split(os.path.realpath(__file__))[0]
     dst = os.path.dirname(os.path.abspath(sys.argv[0]))
-    print(dst)
     cmd = dst + '/start.sh'
-    p = subprocess.Popen(cmd, shell=True,stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    print(cmd)
+    status = subprocess.call(cmd, shell=True)
+    # p = subprocess.Popen(cmd, shell=True,stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
 def start():
+    time.sleep(30)
     upgrade()
+    time.sleep(30)
     run()
+    time.sleep(30)
 
 def upgrade():
     path = get_path()
@@ -25,15 +30,16 @@ def upgrade():
 def cp_file(src):
     # dst = os.path.split(os.path.realpath(__file__))[0]
     dst = os.path.dirname(os.path.abspath(sys.argv[0]))
-    print(dst)
 
     cmd = "cp -r %s %s" % (src, dst)
     print(cmd)
+    # status = subprocess.Popen(cmd, shell=True,stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     status = subprocess.call(cmd, shell=True)
+    '''
     if status != 0:
         # 更新失败，重启
         os.system('init 6')
-
+    '''
 
 def get_path():
     path = '/media/'
